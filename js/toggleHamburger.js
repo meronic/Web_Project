061bc0c1c9a55da344196c1e1bd1c9a0846db84e
meronic <meronic@naver.com>
2020-06-24 14:51:34 +0900

    new site

--- js/toggleHamburger.js
+++
@@ -1,9 +0,0 @@
-// Toggle Hamburger Icon
-(() => {const ul = document.querySelector('.header-nav-lists');
-
-const hamburger = document.querySelector('.header-hamburger-icon');
-
-hamburger.addEventListener('click', () => {
-ul.classList.toggle('show');
-hamburger.classList.toggle('show');
-})})()
